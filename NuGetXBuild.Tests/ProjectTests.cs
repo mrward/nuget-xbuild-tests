fc5785d3d3987fb44266f1558d95ff257a25160a
Matt Ward <ward.matt@gmail.com>
2014-04-08 14:11:00 +0100

    Add more tests for Microsoft.Build.Evaluation.Project.

--- NuGetXBuild.Tests/ProjectTests.cs
+++ NuGetXBuild.Tests/ProjectTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
 using Microsoft.Build.Evaluation;
 using NUnit.Framework;
@@ -21,6 +23,49 @@ namespace NuGetXBuild.Tests
 			string value = project.GetPropertyValue ("TargetFrameworkMoniker");
 
 			Assert.AreEqual (".NETFramework,Version=v4.0", value);
+		}
+
+		[Test]
+		public void ReevaluateIfNecessaryWhenNoChangesMade ()
+		{
+			string xml = @"<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'/>";
+			Project project = new Project (XmlReader.Create (new StringReader (xml)));
+
+			Assert.DoesNotThrow (() => project.ReevaluateIfNecessary ());
+		}
+
+		[Test]
+		public void GetReferenceProjectItemUsingGetItems ()
+		{
+			string xml =
+@"<Project ToolsVersion='4.0' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+	<ItemGroup>
+		<Reference Include='Microsoft.Build' />
+	</ItemGroup>
+</Project>";
+			Project project = new Project (XmlReader.Create (new StringReader (xml)));
+
+			ProjectItem referenceItem = project.GetItems ("Reference").Single ();
+
+			Assert.AreEqual ("Microsoft.Build", referenceItem.EvaluatedInclude);
+		}
+
+		[Test]
+		public void RemoveReferenceFromProject ()
+		{
+			string xml =
+@"<Project ToolsVersion='4.0' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+	<ItemGroup>
+		<Reference Include='Microsoft.Build' />
+	</ItemGroup>
+</Project>";
+			Project project = new Project (XmlReader.Create (new StringReader (xml)));
+			ProjectItem referenceItem = project.GetItems ("Reference").Single ();
+
+			project.RemoveItem (referenceItem);
+
+			int count = project.GetItems ("Reference").Count;
+			Assert.AreEqual (0, count);
 		}
 	}
 }
