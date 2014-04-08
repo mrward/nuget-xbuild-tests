7e3e913e32c12e3b65c47e9d57c87cd110bc9214
Matt Ward <ward.matt@gmail.com>
2014-04-08 13:53:23 +0100

    Add TargetFrameworkMoniker test.
    Test for TargetFrameworkMonitor passed to:
    Microsoft.Build.Evaluation.Project.GetPropertyValue (...)

---
+++ NuGetXBuild.Tests/ProjectTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Microsoft.Build.Evaluation;
+using NUnit.Framework;
+
+namespace NuGetXBuild.Tests
+{
+	[TestFixture]
+	public class ProjectTests
+	{
+		[Test]
+		public void GetTargetFrameworkMonitorPropertyValueFromImportedTargetsFile ()
+		{
+			string xml =
+@"<Project ToolsVersion='4.0' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+	<Import Project='$(MSBuildBinPath)\Microsoft.CSharp.targets' />
+</Project>";
+			Project project = new Project (XmlReader.Create (new StringReader (xml)));
+
+			string value = project.GetPropertyValue ("TargetFrameworkMoniker");
+
+			Assert.AreEqual (".NETFramework,Version=v4.0", value);
+		}
+	}
+}
