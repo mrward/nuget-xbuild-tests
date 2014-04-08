8a5d8c97ad2f2a1c8da1b6709d1a3dbfe3f7e780
Matt Ward <ward.matt@gmail.com>
2014-04-08 15:04:54 +0100

    Add ProjectRootElement tests.
    Microsoft.Build.Construction:
    
    ProjectRootElement.Create(string path) has different behaviour
    to ProjectRootElement.Create (XmlReader reader). When the filename
    is used only the root element is loaded without any child elements.
    When the xml reader is used the entire project is held in the
    returned ProjectRootElement.

---
+++ NuGetXBuild.Tests/ProjectRootElementTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using Microsoft.Build.Construction;
+using System.Linq;
+using System.Xml;
+
+namespace NuGetXBuild.Tests
+{
+	[TestFixture]
+	public class ProjectRootElementTests
+	{
+		[Test]
+		[Ignore("This test does not work on Windows - ProjectRootElement.Create (path) does not take anything inside the root element")]
+		public void CreateUsingFileName ()
+		{
+			string xml =
+				@"<Project ToolsVersion='4.0' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+<ItemGroup>
+	<Reference Include='Microsoft.Build' />
+</ItemGroup>
+</Project>";
+
+			string fileName = CreateProjectXmlFile (xml, "ProjectRootElementTest-CreateUsingFileName.csproj");
+			ProjectRootElement projectElement = ProjectRootElement.Create (fileName);
+
+			File.Delete (fileName);
+
+			ProjectItemElement referenceItem = projectElement.Items.FirstOrDefault (i => i.Include == "Microsoft.Build");
+
+			Assert.IsTrue (projectElement.Items.Count > 0);
+			Assert.AreEqual ("Microsoft.Build", referenceItem.Include);
+		}
+
+		string CreateProjectXmlFile (string xml, string fileName)
+		{
+			string directory = Path.GetDirectoryName (typeof(ProjectTests).Assembly.Location);
+			string fullPath = Path.Combine (directory, fileName);
+
+			File.WriteAllText (fullPath, xml);
+
+			return fullPath;
+		}
+
+		[Test]
+		public void CreateUsingXmlReader ()
+		{
+			string xml =
+				@"<Project ToolsVersion='4.0' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+<ItemGroup>
+	<Reference Include='Microsoft.Build' />
+</ItemGroup>
+</Project>";
+
+			ProjectRootElement projectElement = ProjectRootElement.Create (XmlReader.Create (new StringReader (xml)));
+
+			ProjectItemElement referenceItem = projectElement.Items.FirstOrDefault (i => i.Include == "Microsoft.Build");
+
+			Assert.IsTrue (projectElement.Items.Count > 0);
+			Assert.AreEqual ("Microsoft.Build", referenceItem.Include);
+		}
+	}
+}
+
