d9ad9cd7efcbc6d28d20b481cfc38442d5129795
Matt Ward <ward.matt@gmail.com>
2014-04-11 16:29:43 +0100

    Use fully qualified name in test.
    Makes the test more readable when added to bugzilla without namespace
    statements.

--- NuGetXBuild.Tests/ReevaluateIfNecessaryTests.cs
+++ NuGetXBuild.Tests/ReevaluateIfNecessaryTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml;
-using Microsoft.Build.Evaluation;
 using NUnit.Framework;
 
 namespace NuGetXBuild.Tests
@@ -15,7 +14,7 @@ namespace NuGetXBuild.Tests
 		public void ReevaluateIfNecessaryWhenNoChangesMade ()
 		{
 			string xml = @"<Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003'/>";
-			Project project = new Project (XmlReader.Create (new StringReader (xml)));
+			var project = new Microsoft.Build.Evaluation.Project (XmlReader.Create (new StringReader (xml)));
 
 			Assert.DoesNotThrow (() => project.ReevaluateIfNecessary ());
 		}
