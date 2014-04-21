c6b3aabd12c9d0da20d65680ac5fcd3ef04fb1ce
Matt Ward <ward.matt@gmail.com>
2014-04-21 10:37:21 +0100

    Add test for  Microsoft.Build.Evaluation.Project.ToolsVersion

---
+++ NuGetXBuild.Tests/ProjectToolsVersionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using System.Xml;
+using System.IO;
+
+namespace NuGetXBuild.Tests
+{
+	[TestFixture]
+	public class ProjectToolsVersionTests
+	{
+		[Test]
+		public void GetToolsVersion ()
+		{
+			string xml =
+@"<Project ToolsVersion='4.0' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'>
+</Project>";
+			var project = new Microsoft.Build.Evaluation.Project (XmlReader.Create (new StringReader (xml)));
+			string value = project.ToolsVersion;
+
+			Assert.AreEqual ("4.0", value);
+		}
+	}
+}
+
